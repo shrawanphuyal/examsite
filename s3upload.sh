7b73d3396105a38d32c8eefc14160b84163c81b9
deependra97 <dangaldeependra@gmail.com>
2018-12-16 14:48:39 +0545

    deependra commited

--- s3upload.sh
+++ s3upload.sh
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "deependra97 commited"
+git commit -m "deependra commited"
 git push -u origin deependra
 npm run build
-aws s3 cp /home/deependra97/Documents/practice/attendance-system/build s3://www.examsite.com/ --recursive
+aws s3 cp /home/deependra97/Documents/practice/examsite/build s3://www.examsite.com/ --recursive
